61ae3fe017dfc3ccdf9b422c84f30fc9a5a10c8d
Augusto Cavalcanti <cardoso0guto@gmail.com>
2020-09-06 21:27:39 -0300

    Colore as celulas antes de alterar.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import {Celula} from './celula.model';
 
 @Component({
   selector: 'app-root',
@@ -28,7 +29,7 @@ export class AppComponent {
       this.matriz.push([]);
       for ( let y = 0; y < this.grid; y++) {
         const x = Math.floor((Math.random() * this.limit - 1) + 1);
-        this.matriz[b].push(x);
+        this.matriz[b].push(new Celula(x, 'white'));
       }
     }
   }
@@ -36,7 +37,7 @@ export class AppComponent {
   AlteraCelula() {
     this.colun = Math.floor((Math.random() * this.grid - 1) + 1);
     this.line = Math.floor((Math.random() * this.grid - 1) + 1);
-    this.matriz[this.line][this.colun]++;
+    this.matriz[this.line][this.colun].numero++;
 
     this.Verifica();
   }
@@ -51,13 +52,16 @@ export class AppComponent {
       allRight1 = true;
       cont1 = 0;
       while (allRight1 !== false) {
-        if (this.matriz[cont][cont1] >= this.limit) {
+        if (this.matriz[cont][cont1].numero >= this.limit) {
           allRight = false;
           allRight1 = false;
           this.colun = cont1;
           this.line = cont;
           this.listaDeAvalanches.push('Avalanche! Coluna: ' + (this.colun + 1) + '; Linha: ' + (this.line + 1) + ';');
+          this.AvalancheCor();
           this.buttom = 'prosseguir';
+        } else {
+          this.buttom = 'alterar';
         }
         if (cont1 === (this.grid - 1)) {
           allRight1 = false;
@@ -65,7 +69,6 @@ export class AppComponent {
         cont1++;
       }
       if (cont === (this.grid - 1)) {
-        this.buttom = 'alterar';
         allRight = false;
       }
       cont++;
@@ -73,33 +76,38 @@ export class AppComponent {
   }
 
   editarSuperior() {
-    this.matriz[this.line - 1][this.colun]++;
+    this.matriz[this.line - 1][this.colun].numero++;
+    this.matriz[this.line - 1][this.colun].cor = 'white';
   }
 
   editarInferior() {
-    this.matriz[this.line + 1][this.colun]++;
+    this.matriz[this.line + 1][this.colun].numero++;
+    this.matriz[this.line + 1][this.colun].cor = 'white';
   }
 
   editarEsquerda() {
-    this.matriz[this.line][this.colun - 1]++;
+    this.matriz[this.line][this.colun - 1].numero++;
+    this.matriz[this.line][this.colun - 1].cor = 'white';
   }
 
   editarDireita() {
-    this.matriz[this.line][this.colun + 1]++;
+    this.matriz[this.line][this.colun + 1].numero++;
+    this.matriz[this.line][this.colun + 1].cor = 'white';
   }
 
   editarFoco() {
-    this.matriz[this.line][this.colun] -= 4;
+    this.matriz[this.line][this.colun].numero -= 4;
+    this.matriz[this.line][this.colun].cor = 'white';
   }
 
   Avalanche() {
-    if (this.colun == 0) {
-      if (this.line == 0) {
+    if (this.colun === 0) {
+      if (this.line === 0) {
         this.editarFoco();
         this.editarDireita();
         this.editarInferior();
 
-      } else if (this.line == (this.grid - 1)) {
+      } else if (this.line === (this.grid - 1)) {
         this.editarFoco();
         this.editarDireita();
         this.editarSuperior();
@@ -110,13 +118,13 @@ export class AppComponent {
         this.editarDireita();
         this.editarInferior();
       }
-    } else if (this.colun == (this.grid - 1)) {
-      if (this.line == 0) {
+    } else if (this.colun === (this.grid - 1)) {
+      if (this.line === 0) {
         this.editarFoco();
         this.editarInferior();
         this.editarEsquerda();
 
-      } else if (this.line == (this.grid - 1)) {
+      } else if (this.line === (this.grid - 1)) {
         this.editarFoco();
         this.editarSuperior();
         this.editarEsquerda();
@@ -128,13 +136,13 @@ export class AppComponent {
         this.editarInferior();
       }
     } else {
-      if (this.line == 0) {
+      if (this.line === 0) {
         this.editarFoco();
         this.editarEsquerda();
         this.editarDireita();
         this.editarInferior();
 
-      } else if (this.line == (this.grid - 1)) {
+      } else if (this.line === (this.grid - 1)) {
         this.editarFoco();
         this.editarSuperior();
         this.editarEsquerda();
@@ -150,5 +158,83 @@ export class AppComponent {
     }
 
     this.Verifica();
+  }
+
+  editarCorSuperior() {
+    this.matriz[this.line - 1][this.colun].cor = 'green';
+  }
+
+  editarCorInferior() {
+    this.matriz[this.line + 1][this.colun].cor = 'green';
+  }
+
+  editarCorEsquerda() {
+    this.matriz[this.line][this.colun - 1].cor = 'green';
+  }
+
+  editarCorDireita() {
+    this.matriz[this.line][this.colun + 1].cor = 'green';
+  }
+
+  editarCorFoco() {
+    this.matriz[this.line][this.colun].cor = 'red';
+  }
+
+  AvalancheCor() {
+    if (this.colun === 0) {
+      if (this.line === 0) {
+        this.editarCorFoco();
+        this.editarCorDireita();
+        this.editarCorInferior();
+
+      } else if (this.line === (this.grid - 1)) {
+        this.editarCorFoco();
+        this.editarCorDireita();
+        this.editarCorSuperior();
+
+      } else {
+        this.editarCorFoco();
+        this.editarCorSuperior();
+        this.editarCorDireita();
+        this.editarCorInferior();
+      }
+    } else if (this.colun === (this.grid - 1)) {
+      if (this.line === 0) {
+        this.editarCorFoco();
+        this.editarCorInferior();
+        this.editarCorEsquerda();
+
+      } else if (this.line === (this.grid - 1)) {
+        this.editarCorFoco();
+        this.editarCorSuperior();
+        this.editarCorEsquerda();
+
+      } else {
+        this.editarCorFoco();
+        this.editarCorSuperior();
+        this.editarCorEsquerda();
+        this.editarCorInferior();
+      }
+    } else {
+      if (this.line === 0) {
+        this.editarCorFoco();
+        this.editarCorEsquerda();
+        this.editarCorDireita();
+        this.editarCorInferior();
+
+      } else if (this.line === (this.grid - 1)) {
+        this.editarCorFoco();
+        this.editarCorSuperior();
+        this.editarCorEsquerda();
+        this.editarCorDireita();
+
+      } else {
+        this.editarCorFoco();
+        this.editarCorSuperior();
+        this.editarCorEsquerda();
+        this.editarCorDireita();
+        this.editarCorInferior();
+      }
+    }
   }
 }
---
+++ src/app/celula.model.ts
@@ -0,0 +1,9 @@
+export class Celula {
+  numero: number;
+  cor: string;
+
+  constructor(numero: number, cor: string) {
+    this.numero = numero;
+    this.cor = cor;
+  }
+}
